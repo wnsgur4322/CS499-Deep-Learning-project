de5523ab263d51996a98cd6d284cd3c4ef55a1ac
lockedarmor <ashyan99@gmail.com>
2020-12-06 02:20:07 -0800

    -updated yolo_webcam.py to draw contours

--- yolo_webcam.py
+++ yolo_webcam.py
@@ -2,10 +2,10 @@
 # 1. object detection
 # 2. recollect data with attached camera angle and train it 
 # 3. figure out the size of object https://www.pyimagesearch.com/2016/03/28/measuring-size-of-objects-in-an-image-with-opencv/
-# 	- find pixels per metric. ex) 1 pixel = 0.001 cm
-#	- set up reference distance ex) if distance is 1m between object and camera, then 1 pixel is 0.001 cm
-#	- convert pixels of contour box into size scale.
-# 	- grasping as much as the size scale.
+#	  - find pixels per metric. ex) 1 pixel = 0.001 cm
+#	 - set up reference distance ex) if distance is 1m between object and camera, then 1 pixel is 0.001 cm
+#	 - convert pixels of contour box into size scale.
+#	  - grasping as much as the size scale.
 
 #Written by Junhyeok Jeong
 #!/usr/bin/env python
@@ -23,7 +23,9 @@ import datetime
 import glob
 import math
 import serial
-import syslog
+from sys import platform
+if platform == "linux" or platform == "linux2":
+	import syslog
 import csv
 import os
 
@@ -53,7 +55,7 @@ def distance_to_camera(frame, box):
 def draw_detections(img, rects, thickness = 1):
 	"""
 		INPUT :
-			img  : Gets the input frame
+			img	   : Gets the input frame
 			rect : Number from the regression layer (x0,y0,width,height)
 		OUTPUT:
 			count: Number of objects in a given frame
@@ -79,59 +81,60 @@ def draw_detections(img, rects, thickness = 1):
 
 
 def stack_frames(scale, imgArray):
-    rows = len(imgArray)
-    cols = len(imgArray[0])
-    rowsAvailable = isinstance(imgArray[0], list)
-    width = imgArray[0][0].shape[1]
-    height = imgArray[0][0].shape[0]
-    if rowsAvailable:
-        for x in range ( 0, rows):
-            for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
-                else:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
-                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
-        imageBlank = np.zeros((height, width, 3), np.uint8)
-        hor = [imageBlank]*rows
-        hor_con = [imageBlank]*rows
-        for x in range(0, rows):
-            hor[x] = np.hstack(imgArray[x])
-        ver = np.vstack(hor)
-    else:
-        for x in range(0, rows):
-            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
-                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
-            else:
-                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
-            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-        hor= np.hstack(imgArray)
-        ver = hor
-    return ver
+		rows = len(imgArray)
+		cols = len(imgArray[0])
+		rowsAvailable = isinstance(imgArray[0], list)
+		width = imgArray[0][0].shape[1]
+		height = imgArray[0][0].shape[0]
+		if rowsAvailable:
+				for x in range ( 0, rows):
+						for y in range(0, cols):
+								if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+										imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+								else:
+										imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+								if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+				imageBlank = np.zeros((height, width, 3), np.uint8)
+				hor = [imageBlank]*rows
+				hor_con = [imageBlank]*rows
+				for x in range(0, rows):
+						hor[x] = np.hstack(imgArray[x])
+				ver = np.vstack(hor)
+		else:
+				for x in range(0, rows):
+						if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+								imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+						else:
+								imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+						if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+				hor= np.hstack(imgArray)
+				ver = hor
+		return ver
 
 def get_contours(img, imgContour, frame, x, y, w, h, roi_color):
-    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
-        areaMin = cv2.getTrackbarPos("Area", "Parameters")
-        if area > areaMin:
-            cv2.drawContours(roi_color, cnt, -1, (255, 0, 255), 1)
-            peri = cv2.arcLength(cnt, True)
-            approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
-            print(len(approx))
-            x2 , y2 , w2, h2 = cv2.boundingRect(approx)
-            cv2.rectangle(frame, (x , y), (x + w + pad_w , y + h + pad_h), (0, 255, 0), 1)
+		contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+		for cnt in contours:
+				area = cv2.contourArea(cnt)
+				areaMin = cv2.getTrackbarPos("Area", "Parameters")
+				if area > areaMin:
+						cv2.drawContours(roi_color, cnt, -1, (255, 0, 255), 1)
+						peri = cv2.arcLength(cnt, True)
+						approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
+						print(len(approx))
+						x2 , y2 , w2, h2 = cv2.boundingRect(approx)
+						cv2.rectangle(frame, (x , y), (x + w + pad_w , y + h + pad_h), (0, 255, 0), 1)
 
-            cv2.putText(frame, "Points: " + str(len(approx)), (x + w + 20, y + 20), font, 0.7, (0, 255, 0), 1)
-            cv2.putText(frame, "Area: " + str(int(area)), (x + w + 20, y + 45), font, 0.7, (0, 255, 0), 1)
+						cv2.putText(frame, "Points: " + str(len(approx)), (x + w + 20, y + 20), font, 0.7, (0, 255, 0), 1)
+						cv2.putText(frame, "Area: " + str(int(area)), (x + w + 20, y + 45), font, 0.7, (0, 255, 0), 1)
 
-    return contours
+		return contours
 
 def empty(a):
-    pass
+		pass
 
 
 if __name__ == "__main__":
+	print("start")
 	#termination crietria
 	#criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 	
@@ -176,7 +179,7 @@ if __name__ == "__main__":
 		csvwriter = csv.DictWriter(f, fields)
 		csvwriter.writeheader()
 
-#load YOLO	
+#load YOLO	  
 	if args["dataset"] == "original":
 		net = cv2.dnn.readNet("trained_data/original_yolov3/yolov3.weights","trained_data/original_yolov3/yolov3.cfg") #original Yolov3
 	if args["dataset"] == "tiny":
@@ -201,16 +204,16 @@ if __name__ == "__main__":
 			classes = [line.strip() for line in f.readlines()]
 	if args["dataset"] == "shape":
 		with open("trained_data/shape/obj.names","r") as f:
-		    	classes = [line.strip() for line in f.readlines()]
+					classes = [line.strip() for line in f.readlines()]
 	if args["dataset"] == "46_food":
 		with open("trained_data/46_food/obj.names","r") as f:
-		    	classes = [line.strip() for line in f.readlines()]
+					classes = [line.strip() for line in f.readlines()]
 	if args["dataset"] == "46_food_noised":
 		with open("trained_data/46_noised_food/obj.names","r") as f:
-		    	classes = [line.strip() for line in f.readlines()]
+					classes = [line.strip() for line in f.readlines()]
 	if args["dataset"] == "4_food":
 		with open("trained_data/4_food/obj.names","r") as f:
-		    	classes = [line.strip() for line in f.readlines()]
+				classes = [line.strip() for line in f.readlines()]
 
 	print(classes)
 
@@ -221,9 +224,10 @@ if __name__ == "__main__":
 	colors= np.random.uniform(0,255,size=(len(classes),3))
 
 
-#loading image
+	#loading image
 	#if use open cv 4.2 +, then put cv2.CAP_V4L to prevent gstreaming error
-	cap=cv2.VideoCapture(args["camera_number"], cv2.CAP_V4L) #0 for 1st webcam
+	#cap=cv2.VideoCapture(int(args["camera_number"]), cv2.CAP_V4L) #0 for 1st webcam
+	cap=cv2.VideoCapture(int(args["camera_number"])) #0 for 1st webcam
 	cv2.namedWindow("Parameters")
 	cv2.createTrackbar("Threshold1","Parameters",23,255,empty)
 	cv2.createTrackbar("Threshold2","Parameters",20,255,empty)
@@ -239,17 +243,17 @@ if __name__ == "__main__":
 	distance_in_cm = 0
 	
 	#arduino_data.flush()
-    #arduino_data.write('s'.encode())     #'s', read range once
-	#arduino_data.write('c'.encode())     #'c', read range continuously
-    #arduino_data.write('t'.encode())     #'t', timed measurement
-    #arduino_data.write('.'.encode())     #'.', stop measurement
-    #arduino_data.write('d'.encode())     #'d', dump corrleation record
+		#arduino_data.write('s'.encode())		  #'s', read range once
+	#arduino_data.write('c'.encode())		  #'c', read range continuously
+		#arduino_data.write('t'.encode())		  #'t', timed measurement
+		#arduino_data.write('.'.encode())		  #'.', stop measurement
+		#arduino_data.write('d'.encode())		  #'d', dump corrleation record
 
 
 	assert cap.isOpened(), 'Cannot capture source'
-
+	
 	while cap.isOpened():
-		_,frame= cap.read() # 
+		_,frame= cap.read()
 		frame = cv2.resize(frame, (VIDEO_WIDTH, VIDEO_HEIGHT))
 
 		# imgContour = frame.copy()
@@ -267,11 +271,27 @@ if __name__ == "__main__":
 		height,width,channels = frame.shape
 		
 		#detecting objects
-		blob = cv2.dnn.blobFromImage(frame,0.00392,(VIDEO_WIDTH, VIDEO_HEIGHT),(0,0,0),True,crop=False)    
+		blob = cv2.dnn.blobFromImage(frame,0.00392,(VIDEO_WIDTH, VIDEO_HEIGHT),(0,0,0),True,crop=False)		   
 
 			
 		net.setInput(blob)
 		outs = net.forward(outputlayers)
+
+		#Showing info on screen/ get confidence score of algorithm in detecting an object in blob
+		class_ids=[]
+		confidences=[]
+		boxes=[]
+		for out in outs:
+			for detection in out:
+				scores = detection[5:]
+				class_id = np.argmax(scores)
+				confidence = scores[class_id]
+				if confidence > 0.6:
+					#object detected
+					center_x= int(detection[0]*width)
+					center_y= int(detection[1]*height)
+					w = int(detection[2]*width)
+					h = int(detection[3]*height)
 
 		#Showing info on screen/ get confidence score of algorithm in detecting an object in blob
 		class_ids=[]
@@ -288,7 +308,6 @@ if __name__ == "__main__":
 					center_y= int(detection[1]*height)
 					w = int(detection[2]*width)
 					h = int(detection[3]*height)
-
 					#cv2.circle(img,(center_x,center_y),10,(0,255,0),2)
 					#rectangle co-ordinaters
 					x=int(center_x - w/2)
@@ -311,13 +330,13 @@ if __name__ == "__main__":
 
 		label = None
 
-
+		without_bounding = frame.copy()
 		for i in range(len(boxes)):
 			if i in indexes:
 				print(i)
 				
-				#hog = cv2.HOGDescriptor()   #
-				#hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())    #
+				#hog = cv2.HOGDescriptor()	   #
+				#hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())		 #
 				#found,w = hog.detectMultiScale(frame, winStride=(8,8), padding=(32,32), scale=1.00) #
 				print("found ", str(classes[class_ids[i]])) #
 
@@ -326,12 +345,12 @@ if __name__ == "__main__":
 				get_number_of_object, get_distance= distance_to_camera(frame, boxes[i])
 				#print(get_number_of_object, get_distance)
 				#if get_number_of_object >=1 and get_distance!=0:
-				#	print("{}".format(get_number_of_object)+ " " + classes[class_ids[i]] +" at {}".format(round(get_distance))+" inches")
+				#	 print("{}".format(get_number_of_object)+ " " + classes[class_ids[i]] +" at {}".format(round(get_distance))+" inches")
 				label = str(classes[class_ids[i]])
 				confidence= confidences[i]
 				color = colors[class_ids[i]]
-				pad_w, pad_h = int(0.15*w), int(0.05*h)
-				cv2.rectangle(frame,(x, y), (x+ w + pad_w, y + h + pad_h), color,  1)
+				pad_w, pad_h = int(0.3*w), int(0.05*h)
+				cv2.rectangle(frame,(x, y), (x+ w + pad_w, y + h + pad_h), color,	 1)
 				#cv2.rectangle(frame,(x + pad_w, y + pad_h), (x+w - pad_w, y+h - pad_h), color, 2)
 				
 				cv2.putText(frame, label+" "+str(round(confidence,2)),(x,y-10),font,1,(255,255,255),2)
@@ -347,7 +366,8 @@ if __name__ == "__main__":
 
 				# slice only detected part
 				try:
-					roi_color = frame[y : y + h + pad_h, x : x + w + pad_w]
+					
+					roi_color = without_bounding[y - pad_h : y + h + pad_h, x - pad_w : x + w + pad_w]
 
 					# draw contours part
 					imgContour = roi_color.copy()
@@ -358,9 +378,21 @@ if __name__ == "__main__":
 					canny = cv2.Canny(gray,threshold1,threshold2)
 					kernel = np.ones((5, 5))
 					imgDil = cv2.dilate(canny, kernel, iterations=1)
-					get_contours(imgDil, imgContour, frame, x, y, w, h, roi_color)
+					roi_color = frame[y - pad_h : y + h + pad_h, x - pad_w : x + w + pad_w]
+					contours = get_contours(imgDil, imgContour, frame, x, y, w, h, roi_color)
+					print("len:",len(contours))
+					print("area:",cv2.contourArea(contours[0]))
+					#cv2.drawContours(frame, contours, -1, (0,255,0), 3)
+					"""
+					imgray = cv.cvtColor(without_bounding, cv.COLOR_BGR2GRAY)
+					ret, thresh = cv.threshold(imgray, 127, 255, 0)
+					contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+					
+					
+					"""
 				except:
 					continue
+				
 				# c = max(imgContour, key=cv2.contourArea)
 
 				# # determine the most extreme points along the contour
@@ -385,7 +417,7 @@ if __name__ == "__main__":
 				
 				#read data from LIDAR lite v3 HP
 				#arduino_data.flush()
-				#arduino_data.write('s'.encode())     #'s', read range once
+				#arduino_data.write('s'.encode())		  #'s', read range once
 
 				#read data from US-100 ultrasonic sensor
 				#arduino_data.flush()
@@ -404,11 +436,11 @@ if __name__ == "__main__":
 		cv2.putText(frame,"FPS:"+str(round(fps_cal,2)),(10,50),font,2,(0,0,0),1)
 		cv2.putText(frame, "Press 's' to save data in data.csv", (5, VIDEO_HEIGHT - 20), font, 1, (0,255,0),1)
 		
-	#	if ret == True:
-	#		objpoints.append(objp)
-	#		corners2 = cv2.cornersubPix(frame_calibration, (11, 11), (-1, -1), criteria)
-	#		imgpoints.append(corners2)
-	#		frame_calibration = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
+	#	 if ret == True:
+	#		 objpoints.append(objp)
+	#		 corners2 = cv2.cornersubPix(frame_calibration, (11, 11), (-1, -1), criteria)
+	#		 imgpoints.append(corners2)
+	#		 frame_calibration = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
 
 		cv2.imshow("Video", frame)
 		key = cv2.waitKey(1) #wait 1ms the loop will start again and we will process the next frame
@@ -428,5 +460,5 @@ if __name__ == "__main__":
 
 
 	# check elasped time
-	cap.release()    
+	cap.release()		 
 	cv2.destroyAllWindows()
